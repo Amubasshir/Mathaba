68dd2b6f72cfafda8d805e35ea530b29ebabc4d3
amubasshir <lastmubasshir@gmail.com>
2025-06-04 23:32:52 +0600

    updated wp logo

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { MessageCircle } from 'lucide-react';
-
+import wpIcon from '../public/whatsapp-custom.svg';
 // WhatsApp Floating Button Component
 const WhatsAppButton = () => {
   const handleWhatsAppClick = () => {
@@ -10,10 +10,12 @@ const WhatsAppButton = () => {
   return (
     <button
       onClick={handleWhatsAppClick}
-      className="fixed bottom-20 md:bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white rounded-full p-2 md:p-4 shadow-lg transition-all duration-300 hover:scale-110 z-50"
+    //   className="fixed bottom-20 md:bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white rounded-full p-2 md:p-4 shadow-lg transition-all duration-300 hover:scale-110 z-50"
+      className="fixed bottom-20 md:bottom-6 right-6 text-white rounded-full p-2 md:p-4 shadow-2xl transition-all duration-300 hover:scale-110 z-50"
       aria-label="Contact us on WhatsApp"
     >
-      <MessageCircle size={28} />
+      {/* <MessageCircle size={28} /> */}
+      <img className='h-6 md:h-8 w-6 md:w-8' src={wpIcon.src} alt="" />
     </button>
   );
 };
